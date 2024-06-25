c7d1bb398667b94e2fbe5993ebfc4237a47c1240
st10343093 <st10343093@vcconnect.edu.za>
2024-06-25 11:46:51 +0200

    first commit

---
+++ FoodGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ /*
+ Student Number: ST10343093
+ Full Name: Arshad Bhula
+ */
+
+/*
+=============Code Attribution====================
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/Class_Objects_App
+Date of Access: 25 June 2024 
+Author: Fatima Shaik 
+Website: https://github.com/fb-shaik/PROG6221-Group1-2024/tree/main/EnumDemo_App
+Date of Access: 25 June 2024 
+=============Code Attribution====================
+*/
+
+namespace ProgPart3
+{//namespace begin
+
+    // Added a enum class to hold the 6 food groups
+    public enum FoodGroup
+    {//FoodGroup begin
+        CARBOHYDRATE,
+        PROTEIN,
+        FAT,
+        FRUIT,
+        VEGETABLE,
+        DAIRY
+    }//FoodGroup end enum
+
+}//namespace end
